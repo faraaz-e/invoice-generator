a067a29df4af050f190262727e9bf9947af4fd25
faraaz-e <efaraaz111@gmail.com>
2023-08-24 13:08:32 +0530

    Added Invoice number as document title

--- src/components/UpperSection.js
+++ src/components/UpperSection.js
@@ -17,7 +17,7 @@ const UpperSection = (props) => {
       .join("")
       .slice(0, -3);
     return currentDate + currentTime;
-  };
+  }
 
   useEffect(() => {
     props.sendInvoiceNumber(invoiceNo);
@@ -28,46 +28,52 @@ const UpperSection = (props) => {
     props.sendCurrencyData(currencyType); //send data to parent
   }, [currencyType]);
 
+  function printInvoice(invNo) {
+    document.title = "Invoice-" + invNo;
+    window.print();
+    return false;
+  }
+
   return (
     <>
       <div className="grid grid-cols-4 my-2 items-center print:hidden">
-          <div className="px-1">
-            <select
-              className="p-2 w-full rounded-lg border border-1 border-gray-700 text-xs focus:outline-0"
-              value={currencyType}
-              onChange={(e) => setCurrencyType(e.target.value)}
-            >
-              {currencyList.map((item, index) => (
-                <option className="text-xs" key={index} value={item.value}>
-                  {item.currencyType}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="px-1">
-            <button
-              className="p-2 w-full bg-teal-600 text-white text-xs rounded-lg"
-              onClick={() => setInvoiceNo(generateInvoiceNumber)}
-            >
-              Generate Invoice number
-            </button>
-          </div>
-          <div className="px-1">
-            <button
-              className="p-2 w-full bg-red-500 text-white text-xs rounded-lg"
-              onClick={() => window.location.reload()}
-            >
-              Reset Invoice
-            </button>
-          </div>
-          <div className="px-1">
-            <button
-              className="p-2 w-full bg-blue-600 text-white text-xs rounded-lg"
-              onClick={window.print}
-            >
-              Review / Print Invoice
-            </button>
-          </div>
+        <div className="px-1">
+          <select
+            className="p-2 w-full rounded-lg border border-1 border-gray-700 text-xs focus:outline-0"
+            value={currencyType}
+            onChange={(e) => setCurrencyType(e.target.value)}
+          >
+            {currencyList.map((item, index) => (
+              <option className="text-xs" key={index} value={item.value}>
+                {item.currencyType}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="px-1">
+          <button
+            className="p-2 w-full bg-teal-600 text-white text-xs rounded-lg"
+            onClick={() => setInvoiceNo(generateInvoiceNumber)}
+          >
+            Generate Invoice number
+          </button>
+        </div>
+        <div className="px-1">
+          <button
+            className="p-2 w-full bg-red-500 text-white text-xs rounded-lg"
+            onClick={() => window.location.reload()}
+          >
+            Reset Invoice
+          </button>
+        </div>
+        <div className="px-1">
+          <button
+            className="p-2 w-full bg-blue-600 text-white text-xs rounded-lg"
+            onClick={() => printInvoice(invoiceNo)}
+          >
+            Review / Print Invoice
+          </button>
+        </div>
       </div>
     </>
   );
